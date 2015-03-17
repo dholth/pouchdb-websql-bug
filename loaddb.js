6c0dd536b1e897102bab22162d4cb50ff48f85dd
Daniel Holth <dholth@fastmail.fm>
2015-03-17 14:37:52 -0400

    initial commit

---
+++ loaddb.js
@@ -0,0 +1,56 @@
+/*global PouchDB, Promise, document */
+/*
+ * Populate PouchDB with a few directly fetched test documents.
+ *
+ * This is the last file of one database dump, and all the files from a second 
+ * database dump. IndexedDB winds up with the correct number of documents. 
+ * Chrome WebSQL winds up with too many. Hopefully this WebSQL bug is related
+ * to the real problem we are having.
+ */
+
+ var batches = [[
+ "dump2_00000020.txt",
+ "dump3_00000000.txt",
+ "dump3_00000001.txt",
+ "dump3_00000002.txt",
+ "dump3_00000003.txt",
+ "dump3_00000004.txt",
+ "dump3_00000005.txt",
+ "dump3_00000006.txt",
+ "dump3_00000007.txt",
+ "dump3_00000008.txt",
+ "dump3_00000009.txt",
+ "dump3_00000010.txt",
+ "dump3_00000011.txt",
+ "dump3_00000012.txt",
+ "dump3_00000013.txt",
+ "dump3_00000014.txt",
+ "dump3_00000015.txt",
+ "dump3_00000016.txt",
+ "dump3_00000017.txt",
+ "dump3_00000018.txt",
+ "dump3_00000019.txt",
+ "dump3_00000020.txt"
+]];
+
+
+var db1, db2;
+
+function showInfo(db) {
+  db.info().then(function(info) {
+      $('#info').append("<p id='done'>Have " + info.doc_count + " documents in " + db._name + "</p>");
+  });
+}
+
+$(document).ready(function() {
+  db1 = new PouchDB("loadalot-websql", {adapter:'websql'});
+  db2 = new PouchDB("loadalot-idb", {adapter:'idb'});
+  showInfo(db1);
+  showInfo(db2);
+});
+
+function load(batch, db) {
+  Promise.all(batch.map(function (dumpFile) {
+    return db.load('./' + dumpFile);
+  })).then(function() { showInfo(db); });
+}
